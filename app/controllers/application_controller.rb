212f69e3231364dbd475b2135f310758ab188b32
csdj92 <csdj92@gmail.com>
2020-10-19 09:52:03 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,48 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new 
+  end 
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
   end
+
+  post '/articles' do
+    @articles_show = Article.create(params)
+    redirect "/articles/#{@articles_show.id}"
+    erb :show
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+
+  patch '/articles/:id' do
+    @article_patch = Article.find(params[:id])
+    @article_patch.update(title: params[:title], content: params[:content])
+  
+    redirect "/articles/#{@article_patch.id}"
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.delete
+    
+    redirect "/articles"
+    erb :index
+  end
+
+
 end
