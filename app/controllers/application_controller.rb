f0554af57243a5896de463b7fb375b69a95dce88
doosemavis <moosedavis2011@gmail.com>
2020-10-20 15:41:44 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,14 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do 
+    @articles = Article.all
+    erb :index
+  end 
+
+  get '/articles/:id' do 
+
+  end 
+
 end
